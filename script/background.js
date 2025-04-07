b8a3920fcc1d37020a70d4a1b52fd5ee0a335f36
damien guimiot <damienguimiot@gmail.com>
2025-04-07 09:56:54 +0200

    taille triangle background aléatoire

--- script/background.js
+++ script/background.js
@@ -11,6 +11,8 @@ function createTriangle() {
     triangle.style.rotate = Math.floor(Math.random() * 360) + "deg"
     triangle.style.left = Math.floor(Math.random() * document.documentElement.clientWidth) + "px"
     triangle.style.top = Math.floor(Math.random() * document.documentElement.clientHeight) + "px"
+    triangle.style.width = (Math.floor(Math.random() * 25) + 8) + "px"
+    triangle.style.height = triangle.style.width
     triangle.classList.add("backTriangle")
     return triangle
 }
