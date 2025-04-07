fad9365610896761f233af51eed22dcdbf4b9e9c
damien guimiot <damienguimiot@gmail.com>
2025-04-07 09:51:51 +0200

    position background triangle aléatoire

--- script/background.js
+++ script/background.js
@@ -9,5 +9,8 @@ function createTriangle() {
     let triangle = document.createElement("img")
     triangle.src = "./pictures/triangle.svg"
     triangle.style.rotate = Math.floor(Math.random() * 360) + "deg"
+    triangle.style.left = Math.floor(Math.random() * document.documentElement.clientWidth) + "px"
+    triangle.style.top = Math.floor(Math.random() * document.documentElement.clientHeight) + "px"
+    triangle.classList.add("backTriangle")
     return triangle
 }
